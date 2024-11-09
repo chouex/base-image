55c1f08f13248aa63f54d5f471f51586b82c9a5a
charles chou <767140+chouex@users.noreply.github.com>
2024-11-09 14:35:33 +0800

    Update addon.sh

--- build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/addon.sh
+++ build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/addon.sh
@@ -1,4 +1,6 @@
-IFS=$'\n' 
-for line in $CKPT; do 
-  wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P  "/workspace/storage/stable_diffusion/models/ckpt/" $line;
+#!/bin/bash
+
+for line in ${CKPT//\\n/ } ; do 
+   echo "+ \"$line\""
+   wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P  "/workspace/storage/stable_diffusion/models/ckpt/" $line;
 done
